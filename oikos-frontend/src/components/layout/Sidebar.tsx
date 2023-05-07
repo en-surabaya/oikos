c3c06471056bef80ffd4a950979061915045b5d4
valicaa <xoffel2000@gmail.com>
2023-05-07 18:27:59 +0700

    Mempercantik sidebar

--- oikos-frontend/src/components/layout/Sidebar.tsx
+++ oikos-frontend/src/components/layout/Sidebar.tsx
@@ -6,6 +6,7 @@ import {
   UserGroupIcon,
   UserIcon,
 } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 export const Sidebar: FC = () => {
   return (
@@ -13,19 +14,21 @@ export const Sidebar: FC = () => {
       <div className="flex justify-end text-white">
         <ChevronLeftIcon width={30} />
       </div>
-      <div className="flex items-center justify-start text-white gap-x-4">
+      <div className="flex items-center justify-start text-white gap-x-4 hover:bg-gray-700 active:bg-dark transition rounded-sm p-2">
         <HomeIcon width={30} />
         Dashboard
       </div>
-      <div className="flex items-center justify-start text-white gap-x-4">
-        <UserIcon width={30} />
-        People
-      </div>
-      <div className="flex items-center justify-start text-white gap-x-4">
+      <Link to={"/people"}>
+        <div className="flex items-center justify-start text-white gap-x-4 hover:bg-gray-700 active:bg-dark transition rounded-sm p-2">
+          <UserIcon width={30} />
+          People
+        </div>
+      </Link>
+      <div className="flex items-center justify-start text-white gap-x-4 hover:bg-gray-700 active:bg-dark transition rounded-sm p-2">
         <UserGroupIcon width={30} />
         Lifegroup
       </div>
-      <div className="flex items-center justify-start text-white gap-x-4">
+      <div className="flex items-center justify-start text-white gap-x-4 hover:bg-gray-700 active:bg-dark transition rounded-sm p-2">
         <Cog6ToothIcon width={30} />
         Settings
       </div>
